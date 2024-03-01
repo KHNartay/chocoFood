cc4671d5a5dcbd570392ac2d259449f5e7bd40dd
KHNartay <149667695+KHNartay@users.noreply.github.com>
2024-03-01 16:22:39 +0500

    add cards
    Fri 1 May 2024

--- script.js
+++ script.js
@@ -1,16 +1,62 @@
-async function getFetch() {
-    const url = 'https://65d7137927d9a3bc1d7a1726.mockapi.io/cards'
+async function createCard() {
+    const url = 'https://65d7137927d9a3bc1d7a1726.mockapi.io/cardsObject'
     const response = await fetch(url)
     const data = await response.json()
-    return data;
-}
 
-console.log(getFetch());
+    const cardsDiv = document.querySelector('.cards');
 
-async function createCard() {
-    const cardsDiv = document.querySelector('.cold-foods');
+    // card one
+    const cardOneImg = document.querySelector('.card-one img')
+    cardOneImg.src = data[0].url
 
-    document.querySelector('.card img').src = 
+    document.querySelector('.card-one p').textContent = data[0].name;
+
+    document.querySelector('card-one span').textContent = data[0].price;
+
+    const buttonOne = document.querySelector('.card-one button');
+    buttonOne.addEventListener('click', () => {
+                buttonOne.textContent = 'Added to cart';
+    })
+
+    // card two
+    const cardTwoImg = document.querySelector('.card-two img')
+    cardTwoImg.src = data[1].url
+
+    document.querySelector('.card-two p').textContent = data[1].name;
+
+    document.querySelector('card-one span').textContent = data[1].price;
+
+    const buttonTwo = document.querySelector('.card-two button');
+    buttonTwo.addEventListener('click', () => {
+            buttonTwo.textContent = 'Added to cart';
+    })
+
+    // card three
+    const cardThreeImg = document.querySelector('.card-three img')
+    cardThreeImg.src = data[2].url;
+
+    document.querySelector('.card-three p').textContent = data[2].name;
+
+    document.querySelector('card-one span').textContent = data[2].price;
+
+    const buttonThree = document.querySelector('card-three button');
+    buttonThree.addEventListener('click', () => {
+                buttonThree.textContent = 'Added to cart';
+    })
+
+
+    //card four
+    const cardFourImg = document.querySelector('.card-four img')
+    cardFourImg.src = data[3].url;
+
+    document.querySelector('.card-four p').textContent = data[3].name;
+
+    document.querySelector('card-one span').textContent = data[3].price;
+
+    const buttonFour = document.querySelector('.card-four button');
+    buttonFour.addEventListener('click', () => {
+                    buttonFour.textContent = 'Added to cart';
+    })
 
     return cardsDiv;
 }
